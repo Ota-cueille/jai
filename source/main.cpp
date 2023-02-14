45a30c2fb9d77560379de2c6a6215c3d6bbceeaf
Vianney HERVÉ <vianneyherve3@gmail.com>
2023-02-14 15:19:08 +0100

    read and print file

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,10 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 int main(int ac, char *av[])
 {
-	printf("Hello, World!\n");
-
 	const char *filename;
 	if (ac == 2)
 	{
@@ -13,17 +12,20 @@ int main(int ac, char *av[])
 	}
 	else
 	{
-		printf("no input file was fiven falling back to \"./assets/main.jai\"");
+		printf("no input file was fiven falling back to \"./assets/main.jai\"\n");
 		filename = "assets/main.jai";
 	}
 
 	FILE *source_file = fopen(filename, "r");
 	fseek(source_file, 0, SEEK_END);
 	uint64_t file_size = ftell(source_file);
-	fseek(source_file, 0, SEEK_CUR);
+	fseek(source_file, 0, SEEK_SET);
 
 	char *buffer = (char *)malloc(sizeof(char) * file_size);
+	memset(buffer, '\0', sizeof(char) * file_size);
 	fread(buffer, sizeof(char), file_size, source_file);
+
+	printf(buffer);
 
 	fclose(source_file);
 	free(buffer);
