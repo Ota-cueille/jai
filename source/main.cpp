3e3e0571b0d402c60d15245c645b8ad8834b2dcb
Vianney HERVÉ <vianneyherve3@gmail.com>
2023-03-06 16:47:06 +0100

    source file is read in lexer

--- source/main.cpp
+++ source/main.cpp
@@ -10,29 +10,17 @@ int main(int ac, char *av[])
 	const char *filename;
 	if (ac == 2)
 	{
-		filename = av[1];
+		lexer::set_translation(av[1]);
 	}
 	else
 	{
 		printf("no input file was fiven falling back to \"./assets/main.jai\"\n");
-		filename = "assets/main.jai";
+		lexer::set_translation("./assets/main.jai");
 	}
 
-	FILE *source_file = fopen(filename, "r");
-	fseek(source_file, 0, SEEK_END);
-	uint64_t file_size = ftell(source_file);
-	fseek(source_file, 0, SEEK_SET);
-
-	char *buffer = (char *)malloc(sizeof(char) * file_size);
-	memset(buffer, '\0', sizeof(char) * file_size);
-	fread(buffer, sizeof(char), file_size, source_file);
-
-	printf(buffer);
-
 	// parse file
-
-	fclose(source_file);
-	free(buffer);
+	while (lexer::lookahead.type != lexer::token_type::none)
+		lexer::eat(lexer::lookahead.type);
 
 	return 0;
 }
